2f8f2c8e390d752c0cf1e51b040b3be19fd0bbd2
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-12 08:23:13 -0300

    Department Entity Class

---
+++ src/application/Sistema01.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Department;
+
+public class Sistema01 {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		
+		
+
+	}
+
+}
