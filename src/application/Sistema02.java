5907ac68e69680b96f0e4f31db6a8cf491718556
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-17 15:46:01 -0300

    Update implementation

--- src/application/Sistema02.java
+++ src/application/Sistema02.java
@@ -39,6 +39,17 @@ public class Sistema02 {
 		Seller seller01 = new Seller(12, "Alicinha Leite", "alicinha@gmail.com",Date.valueOf("2021-01-07"), 15500.0, new Department(2, null));
 		sellerDao.insert(seller01);
 		System.out.println("Inserted! New id = " + seller01.getId());
+
+		System.out.println();
+		System.out.println("====== Test 5: Update =======");
+		seller01 = sellerDao.findById(1);
+		seller01.setName("Hugo");
+		seller01.setEmail("Hugo@h.com");
+		seller01.setBirthDate(Date.valueOf("1981-08-20"));
+		seller01.setBaseSalary(30000.00);
+		seller01.setDepartment(new Department(1,null));
+		sellerDao.update(seller01);
+		System.out.println("Update completed!");
 	}
 
 }
